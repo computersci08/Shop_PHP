5ffb50d8a2ae712f49c4522c4860bc2270111327
computersci08 <computersci08@gmail.com>
2024-12-16 22:18:45 +0800

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,11 @@
+﻿<?php include 'inc/header.php';?>
+<?php include 'inc/sidebar.php';?>
+        <div class="grid_10">
+            <div class="box round first grid">
+                <h2> Dashbord</h2>
+                <div class="block">               
+                  <center><h1>"Welcome to the Admin Panel, Ma'am Sheryll Albino! This is the e-commerce website of Group 4 ."</h1></center>      
+                </div>
+            </div>
+        </div>
+<?php include 'inc/footer.php';?>
