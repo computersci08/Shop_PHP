2f5dd9cf134d3bf824f3338f64bf1a3d12c4e3c1
computersci08 <computersci08@gmail.com>
2024-12-16 21:54:42 +0800

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,11 @@
+ <div class="clear">
+        </div>
+    </div>
+    <div class="clear">
+    </div>
+    <div id="site_info">
+       <h3><p> Bryt Shop online Shopping Designed by Jambre, B.</a></h3> 
+        </p>
+    </div>
+</body>
+</html>
